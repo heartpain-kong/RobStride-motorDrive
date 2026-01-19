a2dd8a469e123b795d69f1218b1dc319265e4877
heartpain-kong <2015967035@qq.com>
2026-01-19 12:39:38 +0800

    first save

---
+++ motor_LZ/application/motor_LZ.c
@@ -0,0 +1,311 @@
+#include "motor_LZ.h"
+#include "main.h"
+
+extern CAN_HandleTypeDef hcan1;
+extern CAN_HandleTypeDef hcan2;
+
+static Motor_LZ_Init motor_LZ_id[motor_LZ_N]={
+//				{1,MOTOR_LZ_05},
+				{2,MOTOR_LZ_02}
+};
+
+static uint8_t motor_LZ_id_seat[8];
+static motor_LZ motor_LZ_send_recv[motor_LZ_N];
+
+static CAN_TxHeaderTypeDef  motor_LZ_tx;
+
+static fp32 motor_LZ_max_min(fp32 proto,fp32 max,fp32 min){
+	if(proto>max)return max;
+	else if(proto<min) return min;
+	return proto;
+} 
+
+/**
+  * @brief          灵足电机初始化
+  * @param[in]      none
+  * @retval         none
+  */
+void motor_LZ_init(){
+	can_filter_init();
+	for(uint8_t i=0;i<motor_LZ_N;++i){
+		motor_LZ_send_recv[i].id = motor_LZ_id[i].id;
+		motor_LZ_id_seat[motor_LZ_id[i].id]=i;
+		motor_LZ_send_recv[i].DataHanding.exld.id=motor_LZ_id[i].id;
+		motor_LZ_send_recv[i].DataHanding.exld.res = 0;
+		motor_LZ_send_recv[i].enable = 0;
+		motor_LZ_send_recv[i].model = motor_LZ_id[i].model;
+	}
+	for(uint8_t i=0;i<motor_LZ_N;++i){
+		while(motor_LZ_send_recv[i].Data_recv.Temp==0){
+			motor_LZ_enable(motor_LZ_id[i].id);
+			HAL_Delay(1);
+		}
+		motor_LZ_active_recv(motor_LZ_id[i].id,01);
+		motor_LZ_send_recv[i].enable = 1;
+		motor_LZ_send_recv[i].Data_send->Angle = motor_LZ_send_recv[i].Data_recv.Angle;
+	}
+
+}
+
+/**
+  * @brief          灵足电机外部控制参数获取
+  * @param[in]      id 电机id
+  * @param[in]    	*Data_send 数据指针
+  * @retval         none
+  */
+void motor_LZ_send_init(uint8_t id,user_send_Lz  *Data_send){
+	Data_send->Angle = motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_recv.Angle;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send = Data_send;
+}
+
+/**
+  * @brief          灵足电机返回数据获取
+  * @param[in]      id 电机id
+  * @retval         user_recv_Lz 数据指针
+  */
+const user_recv_Lz *motor_LZ_recv_return(uint8_t id){
+	return &motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_recv;
+}
+
+/**
+  * @brief          灵足电机数据发送
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+static void motor_LZ_Handle_send(uint8_t id){
+	uint32_t motor_LZ_send_box;
+	motor_LZ_tx.ExtId = motor_LZ_send_recv[id].DataHanding.exld.mode<<24
+						|motor_LZ_send_recv[id].DataHanding.exld.data<<8|motor_LZ_send_recv[0].DataHanding.exld.id;
+	motor_LZ_tx.IDE = CAN_ID_EXT ;
+	motor_LZ_tx.RTR = CAN_RTR_DATA ;
+	motor_LZ_tx.DLC = motor_LZ_send_recv[id].DataHanding.len;
+	HAL_CAN_AddTxMessage(&LZ_CAN, &motor_LZ_tx, motor_LZ_send_recv[id].DataHanding.Data, &motor_LZ_send_box);
+
+}
+
+/**
+  * @brief          灵足电机控制数据处理
+  * @param[in]      id 电机id
+  * @param[in]		电机数据处理数组
+  * @param[in]		model电机型号
+  * @retval         none
+  */
+static void motor_LZ_send_handle(uint8_t id,uint16_t *motor,uint8_t model){
+	switch(model){
+		case MOTOR_LZ_02:
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Angle = 
+					motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Angle,12.57f,-12.57f);
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->W =
+								motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->W,44.00f,-44.00f);
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->T =
+								motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->T,17.0f,-17.0f);
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kd =
+								motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kd,5.0f,0.0f);
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kp =
+								motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kp,500.0f,0.0f);
+			motor[0]=(uint16_t)((motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Angle+12.57f)*65535/25.14f);
+			motor[1] = (uint16_t)((motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->W+44.00f)*65535/88.0f);
+			motor[2] = (uint16_t)((motor_LZ_send_recv[id].Data_send->T+17.0f)*65535/34.0f);
+			motor[3] = (uint16_t)((motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kp*65535/500.0f));
+			motor[4] = (uint16_t)((motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kd*65535/5.00f));
+		break;
+		case MOTOR_LZ_05 :
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Angle = 
+					motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Angle,12.57f,-12.57f);
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->W =
+								motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->W,50.00f,-50.00f);
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->T =
+								motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->T,5.5f,-5.5f);
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kd =
+								motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kd,5.0f,0.0f);
+			motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kp =
+								motor_LZ_max_min(motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kp,500.0f,0.0f);
+			motor[0]=(uint16_t)((motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Angle+12.57f)*65535/25.14f);
+			motor[1] = (uint16_t)((motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->W+50.0f)*65535/100.0f);
+			motor[2] = (uint16_t)((motor_LZ_send_recv[id].Data_send->T+5.5f)*65535/11.0f);
+			motor[3] = (uint16_t)((motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kp*65535/500.0f));
+			motor[4] = (uint16_t)((motor_LZ_send_recv[motor_LZ_id_seat[id]].Data_send->Kd*65535/5.0f));
+		break;
+		default:
+		{
+			break;
+		}
+	}
+	
+}
+
+/**
+  * @brief          灵足电机控制数据处理
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+void motor_LZ_send(uint8_t id){
+	uint16_t motor_LZ_send[5];
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.id = id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.mode = CANCOM_MOTOR_CTRL;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.len = 8;
+	
+	motor_LZ_send_handle(id,motor_LZ_send,motor_LZ_send_recv[motor_LZ_id_seat[id]].model);
+	
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.data = motor_LZ_send[2];
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[0]=motor_LZ_send[0]>>8;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[1]=motor_LZ_send[0];
+	
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[2]=motor_LZ_send[1]>>8;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[3]=motor_LZ_send[1];
+	
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[4]=motor_LZ_send[3]>>8;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[5]=motor_LZ_send[3];	
+	
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[6]=motor_LZ_send[4]>>8;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[7]=motor_LZ_send[4];
+	
+	motor_LZ_Handle_send(motor_LZ_id_seat[id]);
+
+}
+
+/**
+  * @brief          灵足电机使能
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+void motor_LZ_enable(uint8_t id){
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.id = id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.mode = CANCOM_MOTOR_IN;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.data = motor_LZ_user_id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.len = 8;
+	for(uint8_t i=0;i<8;++i){
+		motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[i]=0;
+	}
+	motor_LZ_Handle_send(motor_LZ_id_seat[id]);
+
+}
+
+/**
+  * @brief          灵足电机失能
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+void motor_LZ_lose(uint8_t id){
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.id = id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.mode = CANCOM_MOTOR_RESET;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.data = motor_LZ_user_id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.len = 8;
+	for(uint8_t i=0;i<8;++i){
+		motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[i]=0;
+	}
+	motor_LZ_Handle_send(motor_LZ_id_seat[id]);
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].enable = 0;
+}
+
+/**
+  * @brief          灵足电机主动上报帧
+  * @param[in]      id 电机id
+  * @param[in]		F_CMD模式更改 00关 01开启
+  * @retval         none
+  */
+void motor_LZ_active_recv(uint8_t id,uint8_t F_CMD){
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.id = id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.mode = 0x18;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.data = motor_LZ_user_id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.len = 8;
+	for(uint8_t i=0;i<6;++i){
+		motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[i]=i+1;
+	}
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[6]=(F_CMD==1);
+	motor_LZ_Handle_send(motor_LZ_id_seat[id]);
+
+}
+/**
+  * @brief          灵足电机设置机械零点 会把当前电机位置设为机械零位， 会先失能电机, 再使能电机
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+void motor_LZ_zero(uint8_t id){
+	motor_LZ_lose(id);
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.id = id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.mode = CANCOM_MOTOR_ZERO;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.data = motor_LZ_user_id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.len = 8;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[0]=1;
+	
+	motor_LZ_Handle_send(motor_LZ_id_seat[id]);
+	motor_LZ_enable(id);
+}
+/**
+  * @brief          灵足电机设置CAN_ID  失能电机
+  * @param[in]      id 电机id
+  * @param[in] 		修改后（预设）CANID
+  * @retval         none
+  */
+void motor_LZ_set_CAN_ID(uint8_t id,uint8_t set_id){
+	motor_LZ_lose(id);
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.id = id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.mode = CANCOM_MOTOR_ZERO;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.exld.data = set_id;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.len = 8;
+	motor_LZ_send_recv[motor_LZ_id_seat[id]].DataHanding.Data[0]=1;
+	
+	motor_LZ_Handle_send(motor_LZ_id_seat[id]);
+	motor_LZ_enable(id);
+}
+
+/**
+  * @brief          灵足电机返回数据获取
+  * @param[in]      id 电机id
+  * @param[in]		Data ExtId数据传输 
+  * @param[in]		data数组
+  * @param[in]		model电机型号
+  * @retval         none
+  */
+static void motor_LC_recv_data(uint8_t id, uint32_t Data , uint8_t *data,uint8_t model){
+	switch(model){
+		case MOTOR_LZ_02:
+			motor_LZ_send_recv[id].Data_recv.W     = (fp32)(data[2]<<8|data[3])*(fp32)11.0f/8192-44.0f;
+			motor_LZ_send_recv[id].Data_recv.T     = (fp32)(data[4]<<8|data[5])*(fp32)17.0f/32768-17.0f;
+		break;
+		case MOTOR_LZ_05 :
+			motor_LZ_send_recv[id].Data_recv.W     = (fp32)(data[2]<<8|data[3])*(fp32)12.5f/8192-50.0f;
+			motor_LZ_send_recv[id].Data_recv.T     = (fp32)(data[4]<<8|data[5])*(fp32)5.5f/32768-5.5f;
+		break;
+		default:
+		{
+			break;
+		}
+	
+	}
+	motor_LZ_send_recv[id].Data_recv.Angle = (fp32)(data[0]<<8|data[1])*(fp32)12.57f/32768-12.57f;
+	motor_LZ_send_recv[id].Data_recv.Temp  = (fp32)(data[6]<<8|data[7])/10.0f;
+	motor_LZ_send_recv[id].Data_recv.MError= Data>>16&0x3F;
+	motor_LZ_send_recv[id].Data_recv.mode  = Data>>22&0x03;
+}
+ 
+
+
+/**
+  * @brief          can接收回调函数
+  * @param[in]      hcan hcan指针
+  * @retval         none
+  */
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{	
+	if(hcan == &LZ_CAN){
+		
+		CAN_RxHeaderTypeDef rx_header;
+		uint8_t rx_data[8];
+		HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &rx_header, rx_data);
+		uint8_t id = rx_header.ExtId>>8;
+		uint8_t mode = (rx_header.ExtId>>24)&0x1F;
+		switch(mode){
+			case 0x18:
+			case 0x2 :
+				motor_LC_recv_data(motor_LZ_id_seat[id],rx_header.ExtId,rx_data,motor_LZ_send_recv[motor_LZ_id_seat[id]].model);
+			break;
+			default:
+			{
+				break;
+			}
+		}
+	}
+	
+}
---
+++ motor_LZ/application/motor_LZ.h
@@ -0,0 +1,164 @@
+#ifndef __MOTOR_LZ_H__
+#define __MOTOR_LZ_H__
+
+#include "struct_typedef.h"
+
+#include "bsp_can.h"
+
+#define motor_LZ_N 1
+#define motor_LZ_user_id 0
+#define LZ_CAN  hcan1
+
+enum motorModel{
+	MOTOR_LZ_00=0,
+	MOTOR_LZ_01,
+	MOTOR_LZ_02,
+	MOTOR_LZ_03,
+	MOTOR_LZ_04,
+	MOTOR_LZ_05,
+};
+
+enum canComMode{
+	CANCOM_ANNOUNCE_DEVID = 0,//通告设备 ID
+	CANCOM_MOTOR_CTRL, //MOTOR-电机控制
+	CANCOM_MOTOR_FEEDBACK, //MOTOR-电机反馈
+	CANCOM_MOTOR_IN, //MOTOR-进入电机模式
+	CANCOM_MOTOR_RESET, //MOTOR-复位模式
+	CANCOM_MOTOR_CALI, //MOTOR-高速编码器标定
+	CANCOM_MOTOR_ZERO, //MOTOR-设置机械零位
+	CANCOM_MOTOR_ID, //MOTOR-设置 ID
+	CANCOM_PARA_WRITE, //参数-写入
+	CANCOM_PARA_READ, //参数-读取
+	CANCOM_CALI_ING, //编码器标定中
+	CANCOM_CALI_RST, //编码器标定结果
+	CANCOM_PARA_STR_INFO, //参数-字符串信息
+	CANCOM_MOTOR_BRAKE, //MOTOR-进入刹车模式
+	CANCOM_FAULT_WARN, //故障和警告信息
+	CANCOM_MODE_TOTAL, //电机数据保存帧
+	CANCOM_MODE_Bd,		//电机波特率修改帧
+	CANCOM_MODE_ACTIVE_RECV = 0x18,//电机主动上报帧
+	CANCOM_MODE_AGREEMENT,//电机协议修改帧
+};
+
+
+typedef struct __pack
+{
+    uint8_t id    		 :5;      // 电机ID
+    uint8_t model		 :3;
+	
+} Motor_LZ_Init;
+
+typedef struct 
+{
+    uint16_t id    		 :8 ;      // 电机ID
+    uint16_t data  		 :16;      // 数据
+    uint8_t mode :5 ;      // 模式
+	uint8_t  res		 :3 ;	   // 标志位
+	
+} RIS_Mode_t_LZ;
+
+typedef struct {
+	RIS_Mode_t_LZ exld;
+	uint8_t len;		//数据长度
+	uint8_t Data[8]; 	//数据
+}canPack_LZ;
+
+// t_ref=Kd*(v_set-v_actual)+Kp*(p_set-p_actual)+t_ff
+typedef struct{
+	enum canComMode mode;
+	fp32 T;
+	fp32 Angle;
+	fp32 W;	
+	fp32 Kp;
+	fp32 Kd;
+}user_send_Lz;
+
+typedef struct{
+
+	uint8_t MError;
+	uint8_t mode;		//Reset 0  Cali 1  Motor 2;
+	fp32 T;
+	fp32 Angle;
+	fp32 W;
+	fp32 Temp;
+}user_recv_Lz;
+
+
+typedef struct{
+	canPack_LZ    DataHanding;
+	user_send_Lz  *Data_send;
+	user_recv_Lz  Data_recv;
+	uint8_t id;
+	uint8_t enable;				//使能
+	uint8_t model;
+}motor_LZ;
+
+
+/**
+  * @brief          灵足电机初始化
+  * @param[in]      none
+  * @retval         none
+  */
+void motor_LZ_init();
+/**
+  * @brief          灵足电机外部控制参数获取
+  * @param[in]      id 电机id
+  * @param[in]    	*Data_send 数据指针
+  * @retval         none
+  */
+void motor_LZ_send_init(uint8_t id,user_send_Lz  *Data_send);
+/**
+  * @brief          灵足电机返回数据获取
+  * @param[in]      id 电机id
+  * @retval         user_recv_Lz 数据指针
+  */
+const user_recv_Lz *motor_LZ_recv_return(uint8_t id);
+/**
+  * @brief          灵足电机数据发送
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+static void motor_LZ_Handle_send(uint8_t id);
+/**
+  * @brief          灵足电机控制数据处理
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+void motor_LZ_send(uint8_t id);
+
+/**
+  * @brief          灵足电机使能
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+void motor_LZ_enable(uint8_t id);
+
+/**
+  * @brief          灵足电机失能
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+void motor_LZ_lose(uint8_t id);
+/**
+  * @brief          灵足电机主动上报帧
+  * @param[in]      id 电机id
+  * @param[in]		F_CMD模式更改 00关 01开启
+  * @retval         none
+  */
+void motor_LZ_active_recv(uint8_t id,uint8_t F_CMD);
+
+/**
+  * @brief          灵足电机设置机械零点 会把当前电机位置设为机械零位， 会先失能电机, 再使能电机
+  * @param[in]      id 电机id
+  * @retval         none
+  */
+void motor_LZ_zero(uint8_t id);
+/**
+  * @brief          灵足电机设置CAN_ID  失能电机
+  * @param[in]      id 电机id
+  * @param[in] 		修改后（预设）CANID
+  * @retval         none
+  */
+void motor_LZ_set_CAN_ID(uint8_t id,uint8_t set_id);
+
+#endif
